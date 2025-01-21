d32454bb4fcc7afe8951986cf99ed44323ee6f8a
kodexArg <gcavedal@gmail.com>
2025-01-21 22:27:41 +0000

    No more cache (will use redis soon)

--- core/views.py
+++ core/views.py
@@ -5,29 +5,23 @@ import requests
 # Django y DRF
 from django.views.generic import TemplateView
 from django.urls import reverse
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
 from django.conf import settings
 
 # Local
 from .models import SensorData
 from .utils import old_devices_plot_generator, get_start_date, overview_plot_generator, sensor_plot_generator
 
-@method_decorator(cache_page(60), name='dispatch')  # Cache for 1 minute
 class HomeView(TemplateView):
     """Vista principal de la aplicación"""
     template_name = 'home.html'
 
-@method_decorator(cache_page(60), name='dispatch')
 class DevelopmentView(TemplateView):
     """Vista de desarrollo para pruebas"""
     template_name = 'development.html'
 
-@method_decorator(cache_page(60), name='dispatch')
 class ChartsView(TemplateView):
     template_name = "charts.html"
 
-@method_decorator(cache_page(30), name='dispatch')  # Cache for 30 seconds
 class OverviewView(TemplateView):
     template_name = "partials/charts/overview.html"
 
@@ -75,7 +69,6 @@ class OverviewView(TemplateView):
         
         return context
 
-@method_decorator(cache_page(60), name='dispatch')
 class SensorsView(TemplateView):
     template_name = "partials/charts/sensors.html"
 
@@ -114,15 +107,12 @@ class SensorsView(TemplateView):
         context['charts'] = charts
         return context
 
-@method_decorator(cache_page(60), name='dispatch')
 class VPDView(TemplateView):
     template_name = "partials/charts/vpd.html"
 
-@method_decorator(cache_page(30), name='dispatch')
 class GaugesView(TemplateView):
     template_name = "partials/charts/gauges.html"
 
-@method_decorator(cache_page(300), name='dispatch')  # Cache for 5 minutes
 class OldDevicesChartView(TemplateView):
     template_name = 'old-devices.html'
     
