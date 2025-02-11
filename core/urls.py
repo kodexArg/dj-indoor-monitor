bea41579438bfa5479197733cd94258bd2cc52da
kodexArg <gcavedal@gmail.com>
2025-02-11 22:42:30 +0000

    new navbar

--- core/urls.py
+++ core/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('overview/', OverviewView.as_view(), name='overview'),
     path('sensors/', SensorsView.as_view(), name='sensors'),
     path('vpd/', VPDView.as_view(), name='vpd'),
-    path('gauges/', GaugesView.as_view(), name='gauges'),
+    path('charts/gauges/', GaugesView.as_view(), name='gauges'),
     path('', include(router.urls)),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+
+# Local
+from .models import DataPoint
 
 class HomeView(TemplateView):
     template_name = 'development.html'
@@ -7,16 +11,16 @@ class DevelopmentView(TemplateView):
     template_name = 'development.html'
 
 class ChartsView(TemplateView):
-    template_name = 'development.html'
+    template_name = 'charts.html'
 
 class OverviewView(TemplateView):
-    template_name = 'development.html'
+    template_name = 'charts/overview.html'
 
 class SensorsView(TemplateView):
-    template_name = 'development.html'
+    template_name = 'charts/sensors.html'
 
 class VPDView(TemplateView):
-    template_name = 'development.html'
+    template_name = 'charts/vpd.html'
 
 class GaugesView(TemplateView):
-    template_name = 'development.html'
+    template_name = 'charts/gauges.html'
